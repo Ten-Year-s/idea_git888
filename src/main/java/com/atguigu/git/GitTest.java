c57ffbd2ab48db58b9420543dad47f4c17691dfe
liuwei <86786982@qq.com>
2022-06-01 21:31:08 +0800

    新建

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * Date:2022/6/1
+ * Author:LW
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("新建文件" );
+    }
+}
