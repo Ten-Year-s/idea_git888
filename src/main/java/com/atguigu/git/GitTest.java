facbd5bef15ad13feefb3ee942cf52950e5bec1e
liuwei <86786982@qq.com>
2022-06-02 10:02:58 +0800

    第一次更新

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,7 +6,9 @@ package com.atguigu.git;
  * Description:
  */
 public class GitTest {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("新建文件" );
+        System.out.println("第一次更新");
     }
 }
